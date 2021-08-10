96332b6d15dc9d5da01996fc186a838e8a4cb13f
Lakshmi <88740029+LakshmiParamasivan@users.noreply.github.com>
2021-08-10 19:17:54 -0400

    Add files via upload

---
+++ Demodb1Application.java
@@ -0,0 +1,13 @@
+package com.tekarch.demodb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demodb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demodb1Application.class, args);
+	}
+
+}
